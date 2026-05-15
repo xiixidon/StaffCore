3c096e3d0e52c3f9998badcdddd24f4cc3f2e124
Brandon <brandonphan999@gmail.com>
2026-05-15 08:39:34 +0800

    Your descriptive message here

--- hash.php
+++ hash.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 echo "Alice: " . password_hash("Alice@1234", PASSWORD_BCRYPT) . "<br>";
 echo "Bob: " . password_hash("Bob@1234", PASSWORD_BCRYPT) . "<br>";
@@ -6,13 +5,4 @@ echo "Chloe: " . password_hash("Chloe@1234", PASSWORD_BCRYPT) . "<br>";
 echo "David: " . password_hash("David@1234", PASSWORD_BCRYPT) . "<br>";
 echo "Emma: " . password_hash("Emma@1234", PASSWORD_BCRYPT) . "<br>";
 echo "Farid: " . password_hash("Farid@1234", PASSWORD_BCRYPT) . "<br>";
-=======
-<?php
-echo "Alice: " . password_hash("Alice@1234", PASSWORD_BCRYPT) . "<br>";
-echo "Bob: " . password_hash("Bob@1234", PASSWORD_BCRYPT) . "<br>";
-echo "Chloe: " . password_hash("Chloe@1234", PASSWORD_BCRYPT) . "<br>";
-echo "David: " . password_hash("David@1234", PASSWORD_BCRYPT) . "<br>";
-echo "Emma: " . password_hash("Emma@1234", PASSWORD_BCRYPT) . "<br>";
-echo "Farid: " . password_hash("Farid@1234", PASSWORD_BCRYPT) . "<br>";
->>>>>>> 98e1e05841e4235727ebf4e70697bce65fb3fe24
 ?>
